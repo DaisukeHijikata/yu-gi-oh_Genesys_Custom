02dde0af0675deaba1601c1ee21cb943d776f072
dai3 <saiyaman926@gmail.com>
2025-09-28 15:52:44 +0900

    文字列返し APIのルーティング設定３

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/ping', function () {
     return response()->json(['ok' => true, 'time' => now()]);
+});
+Route::post('/echo', function (Request $request) {
+    return response()->json([
+        'received' => $request->all(),
+        'time' => now()
+    ]);
 });
